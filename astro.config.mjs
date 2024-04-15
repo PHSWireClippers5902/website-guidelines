fb2fccaad433995232bebc25447a0a20fbbb6472
lwu877 <67766822+lwu877@users.noreply.github.com>
2024-04-15 15:15:43 -0400

    Update astro.config.mjs
    Added new links to navigation.

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,15 +5,18 @@ import starlight from '@astrojs/starlight';
 export default defineConfig({
 	integrations: [
 		starlight({
-			title: 'My Docs',
+			title: 'Website Documentation',
 			social: {
-				github: 'https://github.com/withastro/starlight',
+				github: 'https://github.com/PHSWireClippers5902',
 			},
 			sidebar: [
 				{
 					label: 'Guides',
 					items: [
 						// Each item here is one entry in the navigation menu.
+						{ label: 'WordPress', link: '/guides/wordpress/' },
+						{ label: 'Colors', link: '/guides/colors/' },
+						{ label: 'Writing', link: '/guides/writing/' },
 						{ label: 'Example Guide', link: '/guides/example/' },
 					],
 				},
